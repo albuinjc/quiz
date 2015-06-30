be35cfaa568a479d82796ce6ee3b03a2eaba25b5
albuinjc <albuinjc@users.noreply.github.com>
2015-06-30 21:22:07 +0200

    P2P Modulo 7: Buscador de preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -15,11 +15,21 @@ exports.load=function( req, res, next, quizId){
 
 //GET /quizes
 exports.index = function(req,res) {
-	models.Quiz.findAll().then(
-		function(quizes) {
-			res.render('quizes/index.ejs', { quizes: quizes });
-		}
-	).catch (function (error) { next(error);});
+	if(req.query.search !== undefined){
+		var search = "%" + req.query.search + "%"; // Comodin % antes y después
+		search = search.trim().replace(/\s/g,"%"); // Sustituimos espacios en blanco por comodin
+		models.Quiz.findAll({where: ["LOWER(pregunta) like ?", search.toLowerCase()], order: 'pregunta ASC'}).then(
+			function(quizes) {
+				res.render('quizes/index.ejs', { quizes: quizes });
+			}
+		).catch (function (error) { next(error);});
+	} else {
+		models.Quiz.findAll().then(
+			function(quizes) {
+				res.render('quizes/index.ejs', { quizes: quizes });
+			}
+		).catch (function (error) { next(error);});
+	}
 };
 
 
