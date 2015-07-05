a294bd59658119ed5134218693dad7cf675ff494
albuinjc <albuinjc@users.noreply.github.com>
2015-07-05 10:40:40 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -72,3 +72,29 @@ exports.create = function (req, res){
 		}
 	);
 };
+
+//GET quizes/:id/edit
+exports.edit = function(req,res){
+	var quiz = req.quiz; //autoload de instancia de quiz
+	
+	res.render('quizes/edit', {quiz: quiz, errors: []});
+};
+
+//PUT /quizes/:id
+exports.update = function (req, res) {
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+	
+	req.quiz.validate().then(
+		function(err){
+			if(err){
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			} else {
+				// Guarda campos pregunta y respuesta en la DB
+				req.quiz.save({fields:["pregunta","respuesta"]}).then(
+					function(){ res.redirect('/quizes');}
+				); // Redirecciona HTTP lista de preguntas (URL relativo)
+			}
+		}	
+	);
+};
