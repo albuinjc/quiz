b3435884c63076898a42e5745f79cb5d761de74a
albuinjc <albuinjc@users.noreply.github.com>
2015-07-07 20:00:24 +0200

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload :quizId
+router.param('commentId', commentController.load); // autoload :commentId
 
 // Definicion de rutas de sesion
 router.get('/login', sessionController.new); // formulario login
@@ -32,6 +33,7 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired,quizContr
 // Comentarios
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', sessionController.loginRequired, commentController.publish);
 
 // Autor
 router.get('/author', authorController.info);
